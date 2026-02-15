a8474d4299e26e20cc1cf30442cfb7090b143ac5
Guy Davies <guydo6davies@gmail.com>
2026-02-15 22:59:00 +0000

    Initial configuration of authentication + User model

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
