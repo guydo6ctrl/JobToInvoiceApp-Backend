ddad674323eb100f8ba25c56cab45d5780071591
Guy Davies <guydo6davies@gmail.com>
2026-02-16 10:42:38 +0000

    Add to installed apps

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "users",
+    "clients",
+    "jobs",
+    "invoices",
+    "quotes",
 ]
 
 REST_FRAMEWORK = {
