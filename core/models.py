7aabc8d2723afb5e68d4a08ad8126e8eb887ed71
Guy Davies <guydo6davies@gmail.com>
2026-02-18 14:53:37 +0000

    Update BaseLineItem to include type

--- core/models.py
+++ core/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 
+class LineItemType(models.TextChoices):
+    LABOUR = "labour", "Labour"
+    MATERIALS = "materials", "Materials"
+    MISCELLANEOUS = "miscellaneous", "Miscellaneous"
+
+
 class BaseLineItem(models.Model):
     description = models.CharField(max_length=255)
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(
+        max_length=20, choices=LineItemType.choices, default=LineItemType.MATERIALS
+    )
 
     class Meta:
         abstract = True
