7583935469568b4ada4ce3aa8a841861bd798992
Guy Davies <guydo6davies@gmail.com>
2026-03-03 11:04:40 +0000

    update user registration

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,40 @@
-from .models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ["username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+            "password2",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "password2": {"write_only": True},
+        }
+
+    def validate(self, data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        validated_data.pop("password2")
+
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
+            password=validated_data["password"],
+        )
+
         return user
